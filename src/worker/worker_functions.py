45fa5bb1306158ec080016e3794f2fef0b491d3f
Max <maximilian-martin.reihn@gmail.com>
2022-01-01 23:27:05 +0100

    changed ip

--- src/worker/worker_functions.py
+++ src/worker/worker_functions.py
@@ -8,23 +8,24 @@ Created on Wed Dec 23 21:40:35 2020
 
 import requests
 import json 
-
+import socket
 
 def get_config():
-    r = requests.get('http://192.168.178.26:8082/api/config')
+    ip_network = socket.gethostbyname(socket.gethostname())
+    r = requests.get('http://%s:8082/api/config'%(ip_network))
     modules = r.json()['data']['modules']
     return [{'name': k['module'], 'config':k['config']} for k in modules if 'config' in k.keys() ]
 
 
 def send_message(title, message , timer):
-    
+    ip_network = socket.gethostbyname(socket.gethostname())
     if timer == '':
         timer = 3000
     
     data = {'title': title, 'message': message, 'timer': 1000*int(timer)}
     headers = {'Content-Type': 'application/json'}
 
-    r = requests.post('http://192.168.178.26:8082/api/module/alert/showalert' , headers = headers, data = json.dumps(data))
+    r = requests.post('http://%s:8082/api/module/alert/showalert'%(ip_network) , headers = headers, data = json.dumps(data))
     
     return True
 
